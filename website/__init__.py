016a5df5d8f6635b2d337695f61d3097295088a5
nurilacka-admin <nuri@adaptivescale.com>
2023-08-11 22:38:50 +0200

    initial commit

---
+++ website/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .views import views
+from .auth import auth
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'haahhsakdakhdhka'
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    return app
+
