91bba66d5f6a879125aad9baf34f42e55cdbd7b0
nurilacka-admin <nuri@adaptivescale.com>
2023-08-12 12:11:42 +0200

    Templates for home and auth

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/signup')
-def signup():
-    return "<p>signup</p>"
+@auth.route('/register')
+def register():
+    return render_template("register.html")
