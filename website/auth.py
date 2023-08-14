b1a032e1edc0ecf05b07def8cff239b909e612b3
nurilacka-admin <nuri@adaptivescale.com>
2023-08-14 16:47:09 +0200

    connect database, register and login

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,32 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .my_helpers import is_valid_email
+from .models import User
+from .database import db
+from flask_login import login_user, current_user
+import bcrypt
+
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", text='testing')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if bcrypt.checkpw(password.encode('utf-8'), user.password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 def logout():
@@ -17,19 +36,28 @@ def logout():
 def register():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password = request.form.get('password')
         confirmPassword = request.form.get('confirmPassword')
 
-        if not is_valid_email(email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif not is_valid_email(email):
             flash("please enter valid email adress", category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password != confirmPassword:
             flash('Passwords don\'t match.', category='error')
         elif len(password) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            new_user = User(email=email, first_name=first_name, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("register.html")
