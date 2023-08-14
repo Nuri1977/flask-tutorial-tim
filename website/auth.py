8a08d4515299e2d068a43209b37871406414230c
nurilacka-admin <nuri@adaptivescale.com>
2023-08-14 11:48:51 +0200

    login and register pages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from .my_helpers import is_valid_email
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", text='testing')
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        if not is_valid_email(email):
+            flash("please enter valid email adress", category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password != confirmPassword:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("register.html")
---
+++ website/my_helpers/__init__.py
@@ -0,0 +1,5 @@
+import re
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
